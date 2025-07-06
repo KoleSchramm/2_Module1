f3eeb228286bb860a1002aae8c9c75de27896998
KoleSchramm <kole.schramm@csuglobal.edu>
2025-07-06 07:22:09 -0500

    Corrections

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String firstname = "";
+        String lastName = "";
+        int accountID = 0;
+        String input = "";
+        Scanner scan = new Scanner(System.in);
+
         //Testing BankAccount
         BankAccount bankAccount = new BankAccount("Kole", "Schramm", 3456745);
         bankAccount.deposit(53723.98);
@@ -26,5 +34,35 @@ public class Main {
         checkingAccount.withdrawal(192628.45);
         checkingAccount.processWithdrawal();
         checkingAccount.displayAccount();
+
+        //Custom CheckingAccount
+        System.out.println("Input first name: ");
+        firstname = scan.next();
+        System.out.println("Input last name: ");
+        lastName = scan.next();
+        System.out.println("Input account ID: ");
+        accountID = scan.nextInt();
+
+        CheckingAccount customAccount = new CheckingAccount(firstname,lastName,accountID);
+        System.out.println("Account created.");
+
+        while (!input.equalsIgnoreCase("end")){
+            System.out.println(String.format("Your balance is: $%.2f",customAccount.balance));
+            System.out.println("Deposit, Withdraw, or End");
+            input = scan.next();
+
+            if(input.equalsIgnoreCase("Deposit")){
+                System.out.println("Input an amount to deposit: ");
+                customAccount.deposit(scan.nextDouble());
+            }
+            else if (input.equalsIgnoreCase("Withdraw")){
+                System.out.println("Input an amount to withdraw: ");
+                customAccount.withdrawal(scan.nextDouble());
+                customAccount.processWithdrawal();
+            }
+            else if (!input.equalsIgnoreCase("End")){
+                System.out.println("Unknown input.");
+            }
+        }
     }
 }
